e46f78950d1a39659e4b9620a710a08d98843bf3
Jakeoid <me@jakeoid.com>
2016-12-24 10:57:50 +1100

    General Improvments
    - Changed `example.jpg` to `example.png`
    - Added more detailing in examples.
    - Added a `LICENSE.md`
    - Added a `PROREADME.md`
    - Some fixes to `README.md`

--- examples/upload_local.rb
+++ examples/upload_local.rb
@@ -1,6 +1,23 @@
+# 
+#                               __    
+# .-----.--.--.--.-----.  .----|  |--.
+# |  _  |  |  |  |  _  |__|   _|  _  |
+# |_____|________|_____|__|__| |_____|
+#                                     
+# Upload Examples
+
+# ############################################################
+
 require '../lib/owo-wrapper'
-
-
 client = OwO::API.new 'YOUR_TOKEN_HERE'
-res = client.upload_local './example.jpg' #or an array, like ['./example.jpg']
+
+# ############################################################
+
+# Select the file in which you want to upload.
+res = client.upload_local './example.png' 
+
+# Can also be in the form of an array.
+res = client.upload_local ['./example.png']
+
+# Start res
 puts res
